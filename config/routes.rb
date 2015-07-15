c3b533a7ff7766975c84027d01f6a57087651c7a
hhe8 <hhe8@uchicago.edu>
2015-07-15 15:59:08 -0500

    make delete and add new restful

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,13 @@ Rails.application.routes.draw do
 
   get '/places' => 'places#index'
   get '/places/new' => 'places#new'
-  get '/places/submit_new' => 'places#submitted'
+  post '/places' => 'places#create'
+
   get '/places/:id' => 'places#show'
-  get '/places/:id/delete' => 'places#delete'
+
+  delete '/places/:id' => 'places#destroy'
   get '/places/:id/edit' => 'places#edit'
   get '/places/:id/submit_edit' => 'places#update'
-
   get '/reviews/:place_id/submit_new'  => 'reviews#create'
 
 end
